2eb719fb735f427d9b7cb3e66bf93c8f760b4e25
marcelvth <37689054+marcelvth@users.noreply.github.com>
2020-06-15 23:40:26 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
-import '../css/app.css';
-import '../css/scss/main.scss';
+
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'popper.js/dist/popper.js';
+
+import '../scss/main.scss';
+
 
